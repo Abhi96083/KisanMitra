29b259968b1a4049fcd4582654d7685896cd6662
Abhinav Kumar <152165255+Abhi96083@users.noreply.github.com>
2025-08-19 22:06:20 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -77,24 +77,22 @@ crop_recommendation_model = pickle.load(
 # Custom functions for calculations
 
 
+from flask import Flask, render_template, request, Markup, redirect  # added redirect
+
 def weather_fetch(city_name):
     """
     Fetch and returns the temperature and humidity of a city
-    :params: city_name
-    :return: temperature, humidity
     """
     api_key = config.weather_api_key
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    complete_url = f"{base_url}appid={api_key}&q={city_name}"
     response = requests.get(complete_url)
     x = response.json()
 
-    if x["cod"] != "404":
-        y = x["main"]
-
-        temperature = round((y["temp"] - 273.15), 2)
-        humidity = y["humidity"]
+    if response.status_code == 200 and "main" in x:
+        temperature = round((x["main"]["temp"] - 273.15), 2)
+        humidity = x["main"]["humidity"]
         return temperature, humidity
     else:
         return None
@@ -262,4 +260,4 @@ def disease_prediction():
 
 # ===============================================================================================
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
